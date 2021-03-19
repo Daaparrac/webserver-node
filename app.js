a4e9fcfc21a4f37f1794375c00cf0d7745fb4a13
Daniel Parra <danielparrac26@gmail.com>
2021-03-18 22:45:58 -0500

    render with handelbars (hbs)

--- app.js
+++ app.js
@@ -1,15 +1,35 @@
 const express = require('express')
+const hbs = require('hbs');
+
 const app = express()
 const port = 8080;
+
+// handlebars
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
 
 //static content
 app.use(express.static('public'))
 
-app.get('/generic', function(req, res) {
-    res.sendFile(__dirname + '/public/generic.html')
+app.get('/', function(req, res) {
+    res.render('home', {
+        nombre: 'Daniel Parra',
+        titulo: 'Learning Node'
+    });
 })
+
+app.get('/generic', function(req, res) {
+    res.render('generic', {
+        nombre: 'Daniel Parra',
+        titulo: 'Learning Node'
+    });
+})
+
 app.get('/elements', function(req, res) {
-    res.sendFile(__dirname + '/public/elements.html')
+    res.render('elements', {
+        nombre: 'Daniel Parra',
+        titulo: 'Learning Node'
+    });
 })
 app.get('/hello-world', function(req, res) {
     res.send('Hello World')
