a93a491ba09f8faffd09ce91e85826b53a6639b7
Daniel Parra <danielparrac26@gmail.com>
2021-03-18 21:50:46 -0500

    Add a static page and configure paths

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 const express = require('express')
 const app = express()
+const port = 8080;
 
-app.get('/', function(req, res) {
-    res.send('home Page')
+//static content
+app.use(express.static('public'))
+
+app.get('/generic', function(req, res) {
+    res.sendFile(__dirname + '/public/generic.html')
 })
-
+app.get('/elements', function(req, res) {
+    res.sendFile(__dirname + '/public/elements.html')
+})
 app.get('/hello-world', function(req, res) {
     res.send('Hello World')
 })
 
-app.listen(8080)
+app.get('*', function(req, res) {
+    res.sendFile(__dirname + '/public/404.html')
+})
+
+app.listen(port, () => {
+    console.log(`example app listening at http://localhost:${port}`);
+})
